ca585f5cdd89bfcffad1d954f458058cc787622a
gu-s <gustandre14@gmail.com>
2016-04-07 22:15:33 +0200

    Added files via upload

--- biblio_Russo/backoffice.php
+++ biblio_Russo/backoffice.php
@@ -1,17 +1,21 @@
-<?php 
-include './autentication.inc';
-include_once './config.php';
-?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-         <meta name="viewport" content="width=device-width, initial-scale=1">
-         <meta name="author" content="Gustavo D.">
-        <title>Biblioteca</title>
-        <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Biblioteca Russo</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
     <!-- Custom CSS -->
+    <link href="css/personale.css" rel="stylesheet">
     <style>
     body {
         padding-top: 70px;
@@ -25,40 +29,60 @@ include_once './config.php';
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    </head>
-    <body>
-        <header>
-        <div id="header">
-           
+
+</head>
+
+<body>
+    <div class="row">
+    <div class="col-lg-12 text-center">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Biblioteca Russo</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <?php include 'nav.php' ?>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
-        </header>
-        
-        <nav id="nav" class="navbar navbar-inverse navbar-fixed-top " role="navigation">            
-            <?php include './nav.php'; ?>           
-        </nav>
-        <!-- Page Content -->
-    <div class="container" id="corpo">
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
         <div class="row">
             <div class="col-lg-12 text-center">
-                <ul class="list-unstyled" id="contenitore">
-                <?php include './cms/inserisci.php'; ?>
-                                  
-                </ul>                    
-            </div>                
+            
+            <?php include './cms/inserisci.php' ?>
+           
+            
+            </div>
         </div>
-        <!-- /.row -->        
-    </div>
-    <!-- /.container -->    
+        <!-- /.row -->
         
-        <footer id="footer" class="footer navbar-inverse navbar-fixed-bottom">
-              <?php include './footer.php'; ?>         
-        </footer>        
-        <!-- /.container -->      
+        <?php include './footer.php' ?>
+    </div>
+    <!-- /.container -->
+
     <!-- jQuery Version 1.11.1 -->
     <script src="js/jquery.js"></script>
+
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/miojavascript.js" type="text/javascript"></script>
-        
-    </body>
+  </div>
+ </div>
+</body>
+
+
 </html>
