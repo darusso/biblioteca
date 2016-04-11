9c927d8c0cd1a580e0ac75225da099cd6152691f
darusso <davide.russo83@gmail.com>
2016-04-11 10:26:51 +0200

    Added files via upload

---
+++ biblio_Russo/cms/Administration.php
@@ -0,0 +1,5 @@
+
+<a href="../../biblio_Russo/cms/inserisci_libro.php">Inserisci libro</a>
+<a href="../../biblio_Russo/cms/inserisci_autore.php">Inserisci autore</a>
+  
+
---
+++ biblio_Russo/cms/inserisci_libro.php
@@ -0,0 +1,66 @@
+<?php
+/*$newURL="index.php?bkoff=1";
+header('Location: '.$newURL);*/
+?>
+<?php
+        //include 'my_library.php';
+        if(isset($_POST['Invio'])){
+            $codice_libro=$_POST['codlibro'];
+            $autore = $_POST['autore'];
+            $titolo =  $_POST['titolo'];
+            $editore = $_POST['editore'];
+            $numero_pagine = $_POST['npagine'];
+            $prezzo = $_POST['prezzo'];
+            $data_acquisto = $_POST['data_acquisto'];
+                               
+            $format="Y-m-d";
+            //$data=date_format($data, $format);
+            if(empty($codice_libro) || empty($autore) || empty($titolo) || empty($editore) || empty($numero_pagine) || empty($prezzo)|| empty($data_acquisto) ){
+                echo "<br>Dati mancanti";
+            }
+            else{                
+                
+                
+                     $conn = mysql_connect("127.0.0.1","biblio","biblio25")
+                        or die("DBMS non disponibile");
+                     mysql_select_db("bibliodb_russo")
+                        or die("DBMS non disponibile");
+                    $query="INSERT INTO libri("
+                    . "codice_libro,autore,titolo,editore,numero_pagine,prezzo,data_acquisto) ";
+                    $query.= "VALUES(NULL,'$codice_libro','$autore','$titolo','$editore','$numero_pagine','$prezzo','$data_acquisto')";
+                    $rows=mysql_query($query,$conn)
+                        or die("Errori in tabella");
+                    if($rows>0){
+                        echo "<br>Registrazione avvenuta correttamente!!<br>";
+                        echo '<a href="index.php">Vai alla Home!</a>';
+                    }
+                    else
+                        echo "<br>Inserimento fallito";
+                    
+                
+            }
+        }
+        //else{
+        ?>
+<form action="backoffice.php?=1" method="post">
+            
+            Codice Libro: <input type="text" name="codlibro" required>
+            <br>
+            Autore: <input type="text" name="autore" required>
+            <br>
+            Titolo: <input type="text" name="titolo" required>
+            <br>
+            Editore: <input type="text" name="editore" required>
+            <br>
+            Numero Pagine:<input type="text" name="npagine" required>
+            <br>
+            Prezzo: <input type="number" name="prezzo" required>
+            <br>
+            Data acquisto:<input type="date" name="data_acquisto" required>
+            <br>
+            <input type="reset" name="cancella" value="cancella">            
+            <input type="submit" name="Invio" value="Invio"> 
+</form>
+
+
+
